ae05d254d88418617ca6309139e8e44b2d2c797e
Hoangit1506 <hoangit1506@gmail.com>
2026-02-05 17:07:38 +0700

    init project

---
+++ src/main/java/com/ma_fashion_vibe_be/MaFashionVibeBeApplication.java
@@ -0,0 +1,13 @@
+package com.ma_fashion_vibe_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaFashionVibeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaFashionVibeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ma_fashion_vibe_be/MaFashionVibeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ma_fashion_vibe_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaFashionVibeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
